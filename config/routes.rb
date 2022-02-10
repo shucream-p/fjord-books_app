6814bfe17e824c9ae3564a017688d8fab31d6921
tominaga <aurora25sh.50nonono@gmail.com>
2022-02-10 13:48:10 +0900

    devisenに対応したコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrantions: "users/registrantions"
+  }
   resources :books
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
