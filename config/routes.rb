fabd5ee547c1145bd84a17eda7775036e456a49b
tominaga <aurora25sh.50nonono@gmail.com>
2022-02-10 16:53:08 +0900

    送信したメールをブラウザで確認できるよう処理を追記

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   }
   resources :books
   resources :users, only: [:index, :show]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
