c4f5da76ec025aedd41e54123285557946b091b2
tominaga <aurora25sh.50nonono@gmail.com>
2022-02-10 19:23:34 +0900

    rubocopで修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name postal_code address introduction])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name postal_code address introduction])
   end
-  
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -44,9 +44,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-    def after_update_path_for(resource)
-      user_path(@user)
-    end
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
