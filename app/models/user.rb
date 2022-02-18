a243b47ccbcb5cfda7d4a3eb6bbd8ab325057e6f
tominaga <aurora25sh.50nonono@gmail.com>
2022-02-18 17:16:21 +0900

    UserとFollowの関連付けを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_follows,
+           class_name: 'Follow',
+           foreign_key: 'follower_id',
+           dependent: :destroy
+  has_many :passive_follows,
+           class_name: 'Follow',
+           foreign_key: 'followed_id',
+           dependent: :destroy
+  has_many :followings, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
 end
