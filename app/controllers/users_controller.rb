247defd7b5d9eb1d848f71419141dac50052dda1
tominaga <aurora25sh.50nonono@gmail.com>
2022-02-22 00:03:05 +0900

    フォロー関連のアクションを追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def followings
+    @users = User.find(params[:id]).followings
+  end
+
+  def followers
+    @users = User.find(params[:id]).followers
+  end
 end
