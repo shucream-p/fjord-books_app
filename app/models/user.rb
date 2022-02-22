63d2c47eed62e6f2920e5d38e21cece32648f136
tominaga <aurora25sh.50nonono@gmail.com>
2022-02-22 19:35:44 +0900

    rubocopで修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@ class User < ApplicationRecord
   has_many :active_follows,
            class_name: 'Follow',
            foreign_key: 'follower_id',
-           dependent: :destroy
+           dependent: :destroy,
+           inverse_of: 'follower'
   has_many :passive_follows,
            class_name: 'Follow',
            foreign_key: 'followed_id',
-           dependent: :destroy
+           dependent: :destroy,
+           inverse_of: 'followed'
   has_many :followings, through: :active_follows, source: :followed
   has_many :followers, through: :passive_follows, source: :follower
 
