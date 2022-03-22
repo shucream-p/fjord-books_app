013e96b52327a76c1877aa9b17f4cb2786dbbbca
tominaga <aurora25sh.50nonono@gmail.com>
2022-03-22 10:07:24 +0900

    不要なrespond_toの記述を削除

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,4 +4,7 @@ class Report < ApplicationRecord
   belongs_to :user
 
   has_many :comments, as: :commentable, dependent: :destroy
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
