0c72b0292eeac316616f27fb451be7354f63c1b0
tominaga <aurora25sh.50nonono@gmail.com>
2022-03-25 19:47:07 +0900

    テストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#following?' do
+    assert_not @alice.following?(@bob)
+    Relationship.create!(following_id: @bob.id, follower_id: @alice.id)
+    assert @alice.following?(@bob)
+  end
+
+  test '#followed_by?' do
+    assert_not @alice.followed_by?(@bob)
+    Relationship.create!(following_id: @alice.id, follower_id: @bob.id)
+    assert @alice.followed_by?(@bob)
+  end
+
+  test '#follow' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+  end
+
+  test '#unfollow' do
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+    @alice.unfollow(@bob)
+    assert_not @alice.following?(@bob)
+  end
+
+  test '#name_or_email' do
+    assert_equal 'alice@example.com', @alice.name_or_email
+
+    @alice.name = 'alice'
+    assert_equal 'alice', @alice.name_or_email
+  end
 end
--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,44 +4,55 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:cherry_book)
+
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: '本'
   end
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
+    click_on '新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'タイトル', with: 'Ruby超入門'
+    fill_in 'メモ', with: 'すごくわかりやすい！！'
+    fill_in '著者', with: 'igaiga'
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '本が作成されました。'
+    assert_text 'Ruby超入門'
+    assert_text 'すごくわかりやすい！！'
+    assert_text 'igaiga'
   end
 
   test 'updating a Book' do
     visit books_url
-    click_on 'Edit', match: :first
+    click_on '編集', match: :prefer_exact
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: '最高です！！'
+    fill_in '著者', with: @book.author
+    click_on '更新する'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text '本が更新されました。'
+    assert_text 'プロを目指す人のためのRuby入門'
+    assert_text '最高です！！'
+    assert_text '伊藤淳一'
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除'
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
